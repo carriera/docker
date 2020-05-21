94abf0738b8a702183ae98988a1fa6845d48ce97
carriera <tomaszlasota@gmail.com>
2020-05-21 09:03:15 +0200

    Update docker-entrypoint.sh

--- php-fpm/etc/docker-entrypoint.sh
+++ php-fpm/etc/docker-entrypoint.sh
@@ -12,7 +12,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
 		php bin/console assets:install
 		php bin/console doctrine:schema:update -f
 		[ -f .messenger_enabled ] && symfony run -d --watch=config,src,templates,vendor symfony console messenger:consume async
-		[ -f .crontab ] && crontab .crontab && cron -f
+		[ -f .crontab ] && crontab .crontab && crond -f
 	fi
 
 	# Permissions hack because setfacl does not work on Mac and Windows
